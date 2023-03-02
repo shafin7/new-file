398c19e371c9fe44b0460f36859bc857c4f3bc2c
shafin <122455234+shafin7@users.noreply.github.com>
2023-03-02 15:00:03 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+ let text=prompt('enter your name')
+ console.log('number of character'
+
+    + text.length);
+
+
+
+  //let text=prompt('enter your mail')
+
+  //console.log('number of character'
+ 
+
+    //+text.length);
